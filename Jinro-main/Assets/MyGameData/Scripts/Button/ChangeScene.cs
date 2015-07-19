5407dadc4059c909e3d3576e2ceb17bc03c3bb5b
T-Kay <ytlol@live.jp>
2015-07-19 17:17:32 +0900

    titleのBaseを作り始めた

---
+++ Jinro-main/Assets/MyGameData/Scripts/Button/ChangeScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class ChangeScene : MonoBehaviour {
+	[SceneName]public string NextSceneName="";
+
+	public void goNext()
+	{
+		if(PhotonNetwork.connected) Application.LoadLevel(NextSceneName);
+	}
+}
